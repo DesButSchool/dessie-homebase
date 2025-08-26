24a992c951f0d8d944478442ac40b348222b8b03
Harper <154289487+O5Desmos@users.noreply.github.com>
2025-08-26 18:36:02 -0400

    update about

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "DessieOwO";
-export const SITE_DESCRIPTION = "Just a silly lil furry calculator.";
+export const SITE_DESCRIPTION = "Just a silly lil furry calculator. UwU";
