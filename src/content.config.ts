594e37e14e5dff095f6ac6c0a0fc8087b6dbf6e4
Dessie at School <hhmcinroe@students.chccs.k12.nc.us>
2025-10-03 14:33:53 -0400

    Remove unused collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,34 +1,4 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
-const music = defineCollection({
-  // Load Markdown and MDX files in the `src/content/games/` directory.
-  loader: glob({ base: "./src/content/music", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    ytVid: z.string(),
-    spotify: z.string().optional(),
-    apple: z.string().optional(),
-    amazon: z.string().optional()
-  }),
-});
-
-export const collections = { blog, music };
+export const collections = {};
